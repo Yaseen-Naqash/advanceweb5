7972b011d37bccdf31746b2be20251888e6f89fd
Yaseen <yasin.naqash.1999@gmail.com>
2026-09-09 19:03:59 +0330

    session 5

--- base/models.py
+++ base/models.py
@@ -1,42 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=127, null=True)
+    def __str__(self):
+        return self.title
 
-
-class Person(models.Model):
-    name = models.CharField(max_length=128, null=True)
-    phone = models.CharField(max_length=128, null=True)
-
-class Book(models.Model):
-    title = models.CharField(max_length=128, null=True)
-    description = models.CharField(max_length=2000, null=True)
-
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
-
-
-class Member(Person):
-    registration_date = models.DateField(null=True)
-
-
-
-class Author(models.Model):
-    age = models.IntegerField(null=True)
-
-
-class Borrow(models.Model):
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    member = models.ForeignKey('Member', on_delete=models.SET_NULL, null=True)
-    date = models.DateField(null=True)
+class Product(models.Model):
+    title = models.CharField(max_length=127, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    price = models.IntegerField(null=True)
+    discount = models.IntegerField(null=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
+    image = models.ImageField(upload_to='/images', null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.member.name + " " + self.book.title + " " + str(self.date)
-
-
-
-
-class Reservation(models.Model):
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    member = models.ForeignKey('Member', on_delete=models.SET_NULL, null=True)
-
-    
+        return self.title
